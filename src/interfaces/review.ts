1f24fb854deeb21ddf404492e5daef43c409660b
AR-Life <ayunus155@gmail.com>
2024-03-06 00:10:14 +0300

    Edit Review interface

--- src/interfaces/review.ts
+++ src/interfaces/review.ts
@@ -1,8 +1,45 @@
 import {type Image} from "./image";
 
-export interface Review{
-    image:Image,
-    fullName: string,
-    comment:string,
-    star:number
-}
+export interface Review {
+    query: string
+    name: string
+    google_id: string
+    place_id: string
+    location_link: string
+    reviews_link: string
+    reviews: string
+    rating: string
+    review_id: string
+    review_pagination_id: string
+    author_link: string
+    author_title: string
+    author_id: string
+    author_image: string
+    author_reviews_count: string
+    author_ratings_count: string
+    review_text: string
+    review_img_urls: string
+    review_img_url: string
+    review_photo_ids: string
+    owner_answer: string
+    owner_answer_timestamp: string
+    owner_answer_timestamp_datetime_utc: string
+    review_link: string
+    review_rating: string
+    review_timestamp: string
+    review_datetime_utc: string
+    review_likes: string
+    reviews_id: string
+    reviews_per_score_1: string
+    reviews_per_score_2: string
+    reviews_per_score_3: string
+    reviews_per_score_4: string
+    reviews_per_score_5: string
+    review_questions_Service: string
+    "review_questions_Meal type": string
+    "review_questions_Price per person": string
+    review_questions_Food: string
+    review_questions_Atmosphere: string
+    review_questions: string
+  }
+  
