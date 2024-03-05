a430865251829c7dfa5af5c79676fbff1703720a
AR-Life <ayunus155@gmail.com>
2024-03-06 00:09:43 +0300

    Add new site Info

--- src/consts/siteInfo.ts
+++ src/consts/siteInfo.ts
@@ -1,8 +1,10 @@
 export const SITE_TITLE = "Gümüşhane Adıyörem Çiğköfte"
 export const SITE_URL = "cigkofte.vercel.app"
 export const PHONE = "+90 (456) 213 61 33"
+export const PHONE_HREF_LINK = "tel:" + PHONE.replaceAll(" ", "").replaceAll("(", "").replaceAll(")", "");
+export const ADDRESS = "Karaer Mahallesi, Atatürk Cd. No:26 Merkez/Gümüşhane"; 
 export const WHATSAPP = ""
 export const BUSSINES_LINK ="https://maps.app.goo.gl/uBnBdxzSn8Tr14RM8"
 export const MAP_LOCALATION = 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12142.952340971413!2d39.4795649!3d40.4589336!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40651b8f7e7bd35f%3A0xa9d681ff779b7ec9!2zR8OcTcOcxZ5IQU5FIMOHxLDEnkvDlkZURSBBRElZw5ZSRU0!5e0!3m2!1str!2str!4v1709455778420!5m2!1str!2str'
-
+export const GET_REVIEW_LINK = "https://g.page/r/Ccl-m3f_gdapEBM/review";
 export const WORK_TIME = {openHour:10, openMinute:0, closeHour:3, closeMinute:0}
