4f0cbbe4024b78c9a49afa96bf5d6867f614ba9d
AR-Life <ayunus155@gmail.com>
2024-03-05 19:47:59 +0300

    add favicon and general settings

--- src/consts/siteInfo.ts
+++ src/consts/siteInfo.ts
@@ -5,4 +5,4 @@ export const WHATSAPP = ""
 export const BUSSINES_LINK ="https://maps.app.goo.gl/uBnBdxzSn8Tr14RM8"
 export const MAP_LOCALATION = 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12142.952340971413!2d39.4795649!3d40.4589336!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40651b8f7e7bd35f%3A0xa9d681ff779b7ec9!2zR8OcTcOcxZ5IQU5FIMOHxLDEnkvDlkZURSBBRElZw5ZSRU0!5e0!3m2!1str!2str!4v1709455778420!5m2!1str!2str'
 
-export const WORK_TIME = {openHour:14, openMinute:0, closeHour:3, closeMinute:0}
+export const WORK_TIME = {openHour:10, openMinute:0, closeHour:3, closeMinute:0}
